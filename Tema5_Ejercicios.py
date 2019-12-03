1d7bcc77f22593da55e706dc4c8f297c561617c9
AlejandroCupul <43485361+AlejandroCupul@users.noreply.github.com>
2019-12-03 07:47:07 -0600

    Ejercicios

---
+++ Tema5_Ejercicios.py
@@ -0,0 +1,36 @@
+import re #libreria de expreciones regulares
+
+path = "Archivo.txt"
+
+try:
+    archivo = open(path, 'r')
+except:
+    print ("El archivo que intentas abrir no existe")
+    quit()
+
+texto = " "
+for linea in archivo:
+         texto += linea
+        
+print (texto)
+
+
+patron = re.compile('[A-Za-z]\w*[ ]*[=][ ]*\w+[.]?\d*')
+result = re.findall(patron,texto)
+print("\n Resultado de busqueda, sentencias de asignación: ", result)
+
+patron2 = re.compile('[A-Za-z]\w*[ ]*[=][ ]*\w+[.]?\d*[ ]*[+-\/*][ ]*\w+')
+result2 = re.findall(patron2,texto)
+print("\n Resultado de busqueda, Operaciones aritméticas básicas: ", result2)
+
+patron3 = re.compile('[A-Za-z]\w*[ ]*[<>!=][=][ ]*[-A-Za-z0-9]+|[A-Za-z]\w*[ ]*[<>][ ]*[-A-Za-z0-9]+')
+result3 = re.findall(patron3,texto)
+print("\n Resultado de busqueda, expresiones booleanas básicas:  ", result3)
+
+patron4 = re.compile('[A-Za-z][\w]*[ ]*[=][ ]*\w+[.]?\d*[ ]*[+-\/*%][ ]*\([ ]*\w+[.]?[\d]*[ ]*[+-\/*%][ ]*\w+[.]?[\d]*[ ]*\)')
+result4 = re.findall(patron4,texto)
+print("\n Resultado de busqueda, Formulas más complejas   ", result4)
+
+patron5 = re.compile('if[ ]*\(.+\)|while[ ]*\(.+\)|for[ ]*\(.+\)')
+result5 = re.findall(patron5,texto)
+print("\n Resultado de busqueda, encabezado de estructuras de control:  ", result5)
